f76bab96ad20a8f892a2e828d50560780dd8b0a8
Aman Kumar Singh Kushwaha <amansinghkushwaha2003@gmail.com>
2025-02-08 01:21:54 +0530

    calendarnav done

--- src/components/CalendarNav.jsx
+++ src/components/CalendarNav.jsx
@@ -1,25 +1,26 @@
 import React, {useState, useContext} from 'react';
 import { CalendarContext } from '../context/CalendarContext';
-import dayjs from 'dayjs';
+import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
+import { format } from 'date-fns';
 import CalendarDialog from './CalenderDialog';
 
 
 const CalendarNav = () => {
   const { currentMonth, setCurrentMonth, setCurrentDate } = useContext (CalendarContext);
 
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isDatepickerOpen, setIsDatepickerOpen] = useState(false);
   const handleMonthClick = () => {
-    setIsDialogOpen(true);
-  }
-  const handleDialogClose = () => {
-    setIsDialogOpen(false);
+    setIsDatepickerOpen(!isDatepickerOpen);
   }
 
-  const handleDateSelect = (date) => {
-    setCurrentMonth(new Date(date.getFullYear(), date.getMonth(), 1));
-    setCurrentDate(date);
 
-    setIsDialogOpen(false);
+  const handleMonthSelect = (date) => {
+    if (date){
+      setCurrentMonth(new Date(date.getFullYear(), date.getMonth(), 1));
+      setCurrentDate(date);
+    }
+
+    setIsDatepickerOpen(false);
   }
 
   // for month toggle button trio
@@ -37,24 +38,38 @@ const CalendarNav = () => {
   };
 
   return (
-    <div className="flex justify-between items-center p-4 border-b">
-      <div className="cursor-pointer" onClick={handleMonthClick}>
-        <h2 className="text-xl font-bold text-blue-600">
-          {dayjs(currentMonth).format("MMMM YYYY")}
-        </h2>
+    <div className="flex justify-between items-center bg-gray-100 py-2 px-4 w-full sticky top-0 z-40">
+      {/* Left wala calender */}
+      <div className="relative">
+        <button
+          className="text-2xl font-regular text-blue-500 hover:opacity-70"
+          onClick={() => handleMonthClick()}
+        >
+          {format(currentMonth, "MMMM yyyy")}
+        </button>
+        {isDatepickerOpen && (
+          <CalendarDialog
+            isOpen={isDatepickerOpen}
+            selectedMonth={currentMonth}
+            onSelectMonth={handleMonthSelect}
+          />
+        )}
       </div>
 
-      <div className='flextems-center gap-2'>
-        <button className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white"onClick={handlePrev}>
-          {'<'}
+      {/* Right wala part */}
+      <div className="flex space-x-4 items-center">
+        <button onClick={handlePrev}>
+          <ChevronLeftIcon color="rgb(59 130 246)" className="hover:opacity-70" />
         </button>
-        <button className="px-4 py-2 bg-green-500 hover:bg-green-600 text-white" onClick={handleToday}>
-          Today  
+        <button
+          className="bg-none text-lg text-blue-500 hover:opacity-70 font-medium"
+          onClick={handleToday}
+        >
+          Today
         </button>
-        <button className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white" onClick={handleNext}>
-          {'>'}
+        <button onClick={handleNext}>
+          <ChevronRightIcon color="rgb(59 130 246)" className="hover:opacity-70" />
         </button>
-
         <button 
           onClick={() => console.log("Add Resource clicked")}
           className="px-4 py-1 bg-green-600 text-white rounded hover:bg-green-700"
@@ -62,12 +77,7 @@ const CalendarNav = () => {
           Add Resource
         </button>
       </div>
-      <CalendarDialog 
-        isOpen={isDialogOpen}
-        onClose={handleDialogClose}
-        selectedDate={currentMonth}
-        onSelectDate={handleDateSelect}
-      />
+      
     </div>
   );
 };
--- src/components/CalenderDialog.jsx
+++ src/components/CalenderDialog.jsx
@@ -2,27 +2,22 @@ import React from 'react';
 import ReactDatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
-const CalendarDialog = ({isOpen, onClose, selectedDate, onSelectDate}) => {
+const CalendarDialog = ({isOpen, selectedMonth, onSelectMonth}) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-centre justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-4 rounded shadow-md">
-        <ReactDatePicker
-          selected={selectedDate}
-          onChange={date => onSelectDate(date)}
-          inline
-          showMonthYearPicker
-        />
-        <button 
-          onClick={onClose} 
-          className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-        >
-          Close
-        </button>
-      </div>
+    <div className="absolute z-50 mt-2" style={{ width: '250px' }}>
+      <div className="w-0 h-0 border-l-8 border-r-8 border-b-8 border-transparent border-b-gray-300 mx-auto"></div>
+      <ReactDatePicker
+        selected={selectedMonth}
+        dateFormat="MMMM/yyyy"
+        onChange={(date) => onSelectMonth(date)}
+        inline
+        showMonthYearPicker
+      />
+      
     </div>
-  )
-}
+  );
+};
 
 export default CalendarDialog;
